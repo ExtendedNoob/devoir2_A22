cea31b480b286f3ded9455431e2dc7f0e34a5619
Frédérick Sorel <fredsorel2005@gmail.com>
2022-11-10 08:50:24 -0500

    Temp generation done 2022-11-10-1

--- js/index.js
+++ js/index.js
@@ -1,40 +1,60 @@
 // Génération de temperature
 
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  
-  const temp = randomIntFromInterval(-20, 30)
-  console.log("La temperature est de " + temp + "°C")
+    function randomIntFromInterval(min, max) { // min and max inclu
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+    
+    const temp = randomIntFromInterval(-20, 30)
+    console.log("La temperature est de " + temp + "°C")
 
 
-// Si la température du jour est entre -20 et 0 alors l’icône neige.jpg est affichée.
+        // Si la température du jour est entre -20 et 0 alors l’icône neige.jpg est affichée.
 
-    if (temp >= -20 && temp <= 0) {
+            if (temp >= -20 && temp <= 0) {
+                document.getElementById("imgNeige").classList.remove("imgHide");
+            }
 
-        console.log("neige.jpg")
+        // Si la température du jour est entre 1 et 10 alors l’icône pluie.jpg est affichée.
+
+            else if (temp >= 1 && temp <= 10) {
+                document.getElementById("imgPluie").classList.remove("imgHide");
+            }
+
+        // Si la température du jour est entre 11 et 20 alors l’icône nuage.jpg est affichée.
+
+            else if (temp >= 11 && temp <= 20) {
+                document.getElementById("imgNuage").classList.remove("imgHide");
+            }
+
+        // Si la température du jour est entre 21 et 30 alors l’icône soleil.jpg est affichée.
+
+            else if (temp >= 21 && temp <= 30) {
+                document.getElementById("imgSoleil").classList.remove("imgHide");
+            }
+
+        // Autre. (Impossible)
+
+            else {
+                console.log("Erreur")
+            }
+
+// Génération de temperature max et min de la journée (entre 0 et 5 ±)
+
+    //Temperature minimal
+
+    function temperatureMinimalJournée(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
-// Si la température du jour est entre 1 et 10 alors l’icône pluie.jpg est affichée.
+    const tempMin = temp - (temperatureMinimalJournée(0, 5))
+    console.log("La temperature min est " + tempMin + "°C")
 
-    else if (temp >= 1 && temp <= 10) {
-        console.log("pluie.jpg")
+    //Temperature maximal
+
+    function temperatureMinimalJournée(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
+    
+    const tempMax = temp + (temperatureMinimalJournée(0, 5))
+    console.log("La tempreature max est " + tempMax + "°C")
 
-// Si la température du jour est entre 11 et 20 alors l’icône nuage.jpg est affichée.
-
-    else if (temp >= 11 && temp <= 20) {
-        console.log("nuage.jpg")
-    }
-
-// Si la température du jour est entre 21 et 30 alors l’icône soleil.jpg est affichée.
-
-    else if (temp >= 21 && temp <= 30) {
-        console.log("soleil.jpg")
-    }
-
-// Autre. (Impossible)
-
-    else {
-        console.log("Erreur")
-    }
