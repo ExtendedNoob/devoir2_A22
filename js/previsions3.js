ede16a72e6a0a305613c69a899d1ffa23a49446b
Frédérick Sorel <fredsorel2005@gmail.com>
2022-11-14 08:54:16 -0500

    Update previsions3.js

--- js/previsions3.js
+++ js/previsions3.js
@@ -1,5 +1,6 @@
-    // Facteur logique: la temprerature ne peut pas passé de -20 à 30 °C dans un délait de 1 jour.
-    // Une différence de + ou - 7 °C sera appliqué entre les jours.
+    // Facteur logique: La temprerature ne peut pas passé de -20 à 30 °C dans un délait de 1 jour.
+    //                  Une différence de + ou - 10 °C sera appliqué entre les jours.
+    //                  La temperature ne dépassera pas -20 ou 30°C.
 // Functions
     function TempInitiale(min, max) { // min and max inclu.
         return Math.floor(Math.random() * (max - min + 1) + min)
@@ -15,22 +16,22 @@
     console.log("tempInitial: " + tempInitiale)
 // Génération temperature des jours suivants.
     // Jour 2
-        const tempJour2 = tempInitiale + (TempJour2(-7, 7))
+        const tempJour2 = tempInitiale + (TempJour2(-10, 10 ))
         if (tempJour2 <= -20) {
-            tempJour2 = "-20"
+            const tempJour2 = -20;
         }
         else if (tempJour2 >= 30) {
-            const tempJour2 = "30"
+            const tempJour2 = 30;
         }
         console.log("TempJour2: " + tempJour2)
     //Jour 3.
-        const tempJour3 = tempJour2 + (TempJour3(-7, 7))
+        const tempJour3 = tempJour2 + (TempJour3(-10, 10))
         if (tempJour3 <= -20) {
-            const tempJour3 = "-20"
+            const tempJour3 = -20;
         }
         else if (tempJour3 >= 30) {
-            const tempJour3 = "30"
+            const tempJour3 = 30;
         }
         console.log("TempJour3: " + tempJour3)
 // Affiché les variables dans le HTML.
-document.getElementById
+document.getElementById("").innerHTML = m;
