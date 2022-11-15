73a2177b2224d6bfc066fbf66ad74d16bff26cd1
Frédérick Sorel <fredsorel2005@gmail.com>
2022-11-15 18:54:17 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
 // Temperature functions.
-    function GenerationTemp(min, max) { // min and max inclu.
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    function GenerationTempMax(min, max) { // min and max inclu.
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-    function GenerationTempMin(min, max) { // min and max inclu.
+    function Generation(min, max) { // min and max inclu.
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 // Génération de temperature.
-    var temp = GenerationTemp(-20, 30)
+    var temp = Generation(-20, 30)
 
         // Si la température du jour est entre -20 et 0 alors l’icône neige.jpg est affichée.
             if (temp >= -20 && temp <= 0) {
@@ -29,9 +23,9 @@
             }
 // Génération de temperature max et min de la journée. (entre 0 et 5 ±)
     //Temperature maximal.
-    const tempMax = temp + (GenerationTempMax(0, 5))
+    const tempMax = temp + (Generation(0, 5))
     //Temperature minimal.
-    const tempMin = temp - (GenerationTempMin(0, 5))
+    const tempMin = temp - (Generation(0, 5))
 // Affiché les variables dans le HTML.
     document.getElementById('tempOutput').innerHTML = temp;
     document.getElementById('tempMaxOutput').innerHTML = tempMax;
