1b82942499c59fde804c9d6c2ee148dae97eb8a9
Frédérick Sorel <fredsorel2005@gmail.com>
2022-11-29 11:16:58 -0500

    fix 2022-11-29

--- js/previsions3.js
+++ js/previsions3.js
@@ -1,7 +1,6 @@
     // Facteur logique: La temprerature ne peut pas passé de -20 à 30 °C dans un délait de 1 jour.
     //                  Une différence de + ou - 15 °C sera appliqué entre les jours.
     //                  La temperature ne dépassera pas -20 ou 30°C.
-    //                  Le Min et Max ne dépassera pas -20 ou 30°C.
 // Function
     function Generation(min, max) { // min and max inclu.
         return Math.floor(Math.random() * (max - min + 1) + min)
@@ -9,9 +8,7 @@
 // Jour 1 (demain)
     var tempInitiale = Generation(-20, 30)
     var tempMax1 = tempInitiale + (Generation(0, 5))
-    var tempMax1 = Math.min(Math.max(parseInt(tempMax1), -20), 30);
     var tempMin1 = tempInitiale - (Generation(0, 5))
-    var tempMin1 = Math.min(Math.max(parseInt(tempMin1), -20), 30);
         if (tempInitiale >= -20 && tempInitiale <= 0) {
             document.getElementById("imgNeige-1").classList.remove("imgHide");
         }
@@ -28,9 +25,7 @@
         var tempJour2 = tempInitiale + (Generation(-15, 15))
         var tempJour2 = Math.min(Math.max(parseInt(tempJour2), -20), 30);
         var tempMax2 = tempJour2 + (Generation(0, 5))
-        var tempMax2 = Math.min(Math.max(parseInt(tempMax2), -20), 30);
         var tempMin2 = tempJour2 - (Generation(0, 5))
-        var tempMin2 = Math.min(Math.max(parseInt(tempMin2), -20), 30);
         if (tempJour2 >= -20 && tempJour2 <= 0) {
             document.getElementById("imgNeige-2").classList.remove("imgHide");
         }
@@ -47,9 +42,7 @@
         var tempJour3 = tempJour2 + (Generation(-15, 15))
         var tempJour3 = Math.min(Math.max(parseInt(tempJour3), -20), 30);
         var tempMax3 = tempJour3 + (Generation(0, 5))
-        var tempMax3 = Math.min(Math.max(parseInt(tempMax3), -20), 30);
         var tempMin3 = tempJour3 - (Generation(0, 5))
-        var tempMin3 = Math.min(Math.max(parseInt(tempMin3), -20), 30);
             if (tempJour3 >= -20 && tempJour3 <= 0) {
                 document.getElementById("imgNeige-3").classList.remove("imgHide");
             }
--- js/previsions7.js
+++ js/previsions7.js
@@ -1,7 +1,6 @@
     // Facteur logique: La temprerature ne peut pas passé de -20 à 30 °C dans un délait de 1 jour.
     //                  Une différence de + ou - 15 °C sera appliqué entre les jours.
     //                  La temperature ne dépassera pas -20 ou 30°C.
-    //                  Le Min et Max ne dépassera pas -20 ou 30°C.
 // Function
     function Generation(min, max) { // min and max inclu.
         return Math.floor(Math.random() * (max - min + 1) + min)
@@ -10,9 +9,7 @@
     // Jour 1 (demain)
         var tempInitiale = Generation(-20, 30)
         var tempMax1 = tempInitiale + (Generation(0, 5))
-        var tempMax1 = Math.min(Math.max(parseInt(tempMax1), -20), 30);
         var tempMin1 = tempInitiale - (Generation(0, 5))
-        var tempMin1 = Math.min(Math.max(parseInt(tempMin1), -20), 30);
             if (tempInitiale >= -20 && tempInitiale <= 0) {
                 document.getElementById("imgNeige-1").classList.remove("imgHide");
             }
@@ -29,9 +26,7 @@
         var tempJour2 = tempInitiale + (Generation(-15, 15))
         var tempJour2 = Math.min(Math.max(parseInt(tempJour2), -20), 30);
         var tempMax2 = tempJour2 + (Generation(0, 5))
-        var tempMax2 = Math.min(Math.max(parseInt(tempMax2), -20), 30);
         var tempMin2 = tempJour2 - (Generation(0, 5))
-        var tempMin2 = Math.min(Math.max(parseInt(tempMin2), -20), 30);
             if (tempJour2 >= -20 && tempJour2 <= 0) {
                 document.getElementById("imgNeige-2").classList.remove("imgHide");
             }
@@ -48,10 +43,7 @@
         var tempJour3 = tempJour2 + (Generation(-15, 15))
         var tempJour3 = Math.min(Math.max(parseInt(tempJour3), -20), 30);
         var tempMax3 = tempJour3 + (Generation(0, 5))
-        var tempMax3 = Math.min(Math.max(parseInt(tempMax3), -20), 30);
         var tempMin3 = tempJour3 - (Generation(0, 5))
-        var tempMin3 = Math.min(Math.max(parseInt(tempMin3), -20), 30);
-
             if (tempJour3 >= -20 && tempJour3 <= 0) {
                 document.getElementById("imgNeige-3").classList.remove("imgHide");
             }
@@ -68,9 +60,7 @@
         var tempJour4 = tempJour3 + (Generation(-15, 15))
         var tempJour4 = Math.min(Math.max(parseInt(tempJour4), -20), 30);
         var tempMax4 = tempJour4 + (Generation(0, 5))
-        var tempMax4 = Math.min(Math.max(parseInt(tempMax4), -20), 30);
         var tempMin4 = tempJour4 - (Generation(0, 5))
-        var tempMin4 = Math.min(Math.max(parseInt(tempMin4), -20), 30);
             if (tempJour4 >= -20 && tempJour4 <= 0) {
                 document.getElementById("imgNeige-4").classList.remove("imgHide");
             }
@@ -87,9 +77,7 @@
         var tempJour5 = tempJour4 + (Generation(-15, 15))
         var tempJour5 = Math.min(Math.max(parseInt(tempJour5), -20), 30);
         var tempMax5 = tempJour5 + (Generation(0, 5))
-        var tempMax5 = Math.min(Math.max(parseInt(tempMax5), -20), 30);
         var tempMin5 = tempJour5 - (Generation(0, 5))
-        var tempMin5 = Math.min(Math.max(parseInt(tempMin5), -20), 30);
             if (tempJour5 >= -20 && tempJour5 <= 0) {
                 document.getElementById("imgNeige-5").classList.remove("imgHide");
             }
@@ -106,9 +94,7 @@
         var tempJour6 = tempJour5 + (Generation(-15, 15))
         var tempJour6 = Math.min(Math.max(parseInt(tempJour6), -20), 30);
         var tempMax6 = tempJour6 + (Generation(0, 5))
-        var tempMax6 = Math.min(Math.max(parseInt(tempMax6), -20), 30);
         var tempMin6 = tempJour6 - (Generation(0, 5))
-        var tempMin6 = Math.min(Math.max(parseInt(tempMin6), -20), 30);
             if (tempJour6 >= -20 && tempJour6 <= 0) {
                 document.getElementById("imgNeige-6").classList.remove("imgHide");
             }
@@ -125,9 +111,7 @@
         var tempJour7 = tempJour6 + (Generation(-15, 15))
         var tempJour7 = Math.min(Math.max(parseInt(tempJour7), -20), 30);
         var tempMax7 = tempJour7 + (Generation(0, 5))
-        var tempMax7 = Math.min(Math.max(parseInt(tempMax7), -20), 30);
         var tempMin7 = tempJour7 - (Generation(0, 5))
-        var tempMin7 = Math.min(Math.max(parseInt(tempMin7), -20), 30);
             if (tempJour7 >= -20 && tempJour7 <= 0) {
                 document.getElementById("imgNeige-7").classList.remove("imgHide");
             }
